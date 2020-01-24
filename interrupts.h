a1b79141aae8a214d8170483fbd0837a1dc2573f
Az-dev <aliali.elshazly@gmail.com>
2020-01-24 10:41:02 +0200

    added interrupts.h

---
+++ interrupts.h
@@ -0,0 +1,10 @@
+void __attribute__ ((interrupt)) TIMER0_OVF_vect(void);
+void __attribute__ ((interrupt)) TIMER0_COMP_vect(void);
+/*
+void __attribute__ ((interrupt)) T1_OV_ISR(void);
+void __attribute__ ((interrupt)) T1_OCA_ISR(void);
+void __attribute__ ((interrupt)) T1_OCB_ISR(void);
+void __attribute__ ((interrupt)) T1_IC_ISR(void);
+void __attribute__ ((interrupt)) T2_OV_ISR(void);
+void __attribute__ ((interrupt)) T2_OC_ISR(void);
+*/
